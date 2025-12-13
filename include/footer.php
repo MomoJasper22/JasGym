7cab1f4df6cad87e13410ffb4e7340346a27db8e
Helloword <jaspermomo1@gmail.com>
2025-12-13 15:16:25 +0800

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,14 @@
+<footer class="">
+		<div class="container">
+			
+			<div class="footer-bottom">
+				<div class="row">
+					
+					<div class="col-md-8 text-md-right">
+						<div class="copyright"> 
+JasGym Smart Tool for Unstoppable Gyms </div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
